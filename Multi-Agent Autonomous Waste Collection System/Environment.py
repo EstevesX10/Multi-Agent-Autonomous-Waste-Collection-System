f193a850ada656b9d37a31acd0cac8ff6b0371c2
EstevesX10 <goncalo.mesteves03@gmail.com>
2024-10-05 23:17:09 +0100

    Added imports

---
+++ Multi-Agent Autonomous Waste Collection System/Environment.py
@@ -0,0 +1,11 @@
+# Import necessary SPADE modules
+from spade.agent import Agent
+from spade.behaviour import (CyclicBehaviour, OneShotBehaviour, PeriodicBehaviour)
+from spade.template import (Template)
+from spade.message import (Message)
+import asyncio
+import spade
+
+class Environment():
+    def __init__(self) -> None:
+        pass
