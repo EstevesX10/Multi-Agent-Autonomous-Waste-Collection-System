b329ac3d65b03f611a7c299c1b5862f6bdc71530
EstevesX10 <goncalo.mesteves03@gmail.com>
2024-10-07 09:59:21 +0100

    Not much

--- Multi-Agent Autonomous Waste Collection System/Environment.py
+++ Multi-Agent Autonomous Waste Collection System/Environment.py
@@ -1,11 +1,21 @@
 # Import necessary SPADE modules
-from spade.agent import Agent
+from spade.agent import (Agent)
 from spade.behaviour import (CyclicBehaviour, OneShotBehaviour, PeriodicBehaviour)
 from spade.template import (Template)
 from spade.message import (Message)
 import asyncio
 import spade
 
+# Maybe use a Graph
+from .DataStructures import (Graph)
+
+# from .BinAgent import (BinAgent)
+# from .TruckAgent import (TruckAgent)
+
 class Environment():
     def __init__(self) -> None:
+        self.graph = Graph(10)
+        self._readGraph()
+    
+    def _readGraph(self):
         pass
