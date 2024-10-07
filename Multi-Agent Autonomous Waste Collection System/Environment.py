6b9de2364e465419761062aff106bd54bf85724b
EstevesX10 <goncalo.mesteves03@gmail.com>
2024-10-07 18:07:11 +0100

    Started populating a graph with some data

--- Multi-Agent Autonomous Waste Collection System/Environment.py
+++ Multi-Agent Autonomous Waste Collection System/Environment.py
@@ -7,15 +7,57 @@ import asyncio
 import spade
 
 # Maybe use a Graph
-from .DataStructures import (Graph)
+from DataStructures import (Graph)
 
 # from .BinAgent import (BinAgent)
 # from .TruckAgent import (TruckAgent)
 
+class Road():
+    # Type of Value I am thinking of putting inside the connections of the graph
+    def __init__(self, distance, availability, fuelConsumption, batteryConsumption) -> None:
+        self._distance = distance
+        self._availability = availability
+        self._fuelConsumption = fuelConsumption
+        self._batteryConsumption = batteryConsumption
+    
+    def getDistance(self):
+        return self._distance
+    
+    def getAvailability(self):
+        return self._availability
+    
+    def getFuelConsumption(self):
+        return self._fuelConsumption
+    
+    def getBatteryConsumption(self):
+        return self._batteryConsumption
+    
+    def toggleAvailability(self):
+        self._availability = 0 if self._availability == 1 else 1
+
 class Environment():
     def __init__(self) -> None:
-        self.graph = Graph(10)
-        self._readGraph()
-    
-    def _readGraph(self):
-        pass
+        self.graph = self._readGraph('./EnvironmentLayouts/Layout1.txt')
+
+    def _readGraph(self, envConfiguration:str) -> Graph:
+        # Open the file in read mode
+        with open(envConfiguration, 'r') as f:
+            # Seperate the 
+            lines = f.read().split('\n')
+            
+            for idx, line in enumerate(lines):
+                if (idx == 0):
+                    numberNodes = int(line)
+                    self.graph = Graph(numberNodes)
+                elif(idx == 1):
+                    numberEdges = int(line)
+                else:
+                    for _ in range(numberEdges):
+                        startNode, endNode, distance, availability, fuelConsumption, batteryConsumption = map(int, line.split(' '))
+                        newRoad = Road(distance, availability, fuelConsumption, batteryConsumption)
+                        self.graph.insertNewEdge(startNode, endNode, newRoad)
+
+        print(self.graph.numEdges())
+
+if __name__ == "__main__":
+    env = Environment()
